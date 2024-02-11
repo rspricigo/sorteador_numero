317df305d1a8f85e63d02a079173a970e3f823b7
Reginaldo Spricigo <reginaldospricigo@gmail.com>
2024-02-11 13:14:43 -0300

    Implementando o reiniciar

--- app.js
+++ app.js
@@ -20,10 +20,35 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados.sort((a,b) => a - b)}</label>`;
-    
+    abilitaBotao();
     
 }
 
+
+function abilitaBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+}
+
+
+function desabilitaBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao')){
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').value = '<label class="texto__paragrafo">Números sorteados:  nenhum ate agora</label>';
+    desabilitaBotao();
+}
 
 
 
