a965e7afcbed5b7de0658d53c58cb2cd0ca8816b
Reginaldo Spricigo <reginaldospricigo@gmail.com>
2024-02-11 17:35:20 -0300

    Prenenindo erros

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+    
 
     let sorteados = [];
     let numero;
